cbace1c34d0b0cf40cbb8f050f3bf205096af962
nsangou610 <hello@gmail.com>
2024-10-16 23:02:42 +0100

    modification du fichier ruls.py et ajout du dossier userapp avce ses fichiers

---
+++ userapp/serializers.py
@@ -0,0 +1,71 @@
+from rest_framework import serializers
+from userapp.models import User
+from django.contrib.auth import authenticate
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["email", "username", "password", "confirm_password"]
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        confirm_password = attrs.get('confirm_password')
+
+        if password != confirm_password:
+            raise serializers.ValidationError("Password and Confirm_Password doesn't match.")
+        return attrs
+    
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+          raise serializers.ValidationError('user with this Email already exists.')
+        return value
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+
+        user.save()
+        return user
+    
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.save()
+        return instance
+    
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+    
+    def check_user(self, clean_data):
+        email = clean_data['email']
+        password = clean_data['password']
+        user=authenticate(email=email, password=password)
+        if not user:
+            raise ValueError("user not found")
+        return user
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
